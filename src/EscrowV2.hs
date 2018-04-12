f6c08cc216614312b6b0551a38118775a1f89f55
Simon Thompson <s.j.thompson@kent.ac.uk>
2018-04-12 14:46:31 +0100

    Added EscrowV2.hs, uses embedding of Contract in Haskell.

---
+++ src/EscrowV2.hs
@@ -0,0 +1,62 @@
+module Escrow where
+
+import Semantics
+
+------------------------------------------
+-- Implementation of an escrow contract --
+------------------------------------------
+
+-- The contract allows person 1 to pay 450 ADA
+-- to person 2 by using person 3 as an escrow.
+--
+-- Person 1 can commit 450 ADA until block 100.
+-- The payment will go through only if 2 out of the 3
+-- participants choose the number "1".
+--
+-- If 2 out of the 3 participants chooses the number "0"
+-- or if payment did not go through after block 100,
+-- the money will be refunded to person 1.
+
+escrow :: Contract
+escrow = CommitCash iCC1 1
+                    (ConstMoney 450)
+                    10 100
+                    (When (OrObs (two_chose 1 2 3 0)
+                                 (two_chose 1 2 3 1))
+                          90
+                          (Choice (two_chose 1 2 3 1)
+                                  (Pay iP1 1 2
+                                       (AvailableMoney iCC1)
+                                       100
+                                       redeem_original)
+                                   redeem_original)
+                          redeem_original)
+                    Null
+
+chose :: Int -> ConcreteChoice -> Observation
+
+chose per c = 
+        PersonChoseThis (IdentChoice per) per c
+
+one_chose :: Person -> Person -> ConcreteChoice -> Observation
+
+one_chose per per' val = 
+        (OrObs (chose per val) (chose per' val)) 
+                                  
+two_chose :: Person -> Person -> Person -> ConcreteChoice -> Observation
+
+two_chose p1 p2 p3 c =
+        OrObs (AndObs (chose p1 c) (one_chose p2 p3 c))
+              (AndObs (chose p2 c) (chose p3 c))
+
+redeem_original :: Contract
+
+redeem_original = RedeemCC iCC1 Null
+
+iCC1 :: IdentCC
+
+iCC1 = IdentCC 1
+
+iP1 :: IdentPay
+
+iP1 = IdentPay 1

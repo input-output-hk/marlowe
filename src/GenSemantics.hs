e875756b09e9f3fe20d755e52eb9389ca9dd68bf
Pablo Lamela Seijas <palas87@gmail.com>
2018-07-25 15:52:10 +0100

    Implement generator for contracts

--- src/GenSemantics.hs
+++ src/GenSemantics.hs
@@ -36,4 +36,18 @@ arbitraryObservationAux s
 arbitraryObservation :: Gen Observation
 arbitraryObservation = sized arbitraryObservationAux
 
+arbitraryContractAux :: Int -> Gen Contract
+arbitraryContractAux s
+ | s > 0 = oneof [pure Null
+                 ,(CommitCash . IdentCC) <$> arbitrary <*> arbitrary <*> arbitraryMoneyAux (s - 1) <*> arbitrary <*> arbitrary <*> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1) 
+                 ,(RedeemCC . IdentCC) <$> arbitrary <*> arbitraryContractAux (s - 1)
+                 ,(Pay . IdentPay) <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitraryMoneyAux (s - 1) <*> arbitrary <*> arbitraryContractAux (s - 1)
+                 ,Both <$> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1)
+                 ,Choice <$> arbitraryObservationAux (s - 1) <*> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1)
+                 ,When <$> arbitraryObservationAux (s - 1) <*> arbitrary <*> arbitraryContractAux (s - 1) <*> arbitraryContractAux (s - 1)]
+ | s == 0 = oneof [pure Null]
+ | otherwise = error "Negative size in arbitraryObservation"
+
+arbitraryContract :: Gen Contract
+arbitraryContract = sized arbitraryContractAux
 

51ddb9297cc4d3d8e25d7b0fbaccc967294fbcce
Alexander Nemish <anemish@gmail.com>
2018-10-12 15:26:05 +0300

    Add basic tests

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,9 @@ import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import qualified Data.Maybe as Maybe
 import Control.Monad.State
+import Test.Tasty
+import Test.Tasty.QuickCheck as QC
+import Test.Tasty.HUnit
 
 import Semantics as S
 import SPJModel hiding (Contract(..))
@@ -12,6 +15,31 @@ import CrowdFunding
 import DepositIncentive
 import Escrow
 
+evaluateMaximumValueTests :: [TestTree]
+evaluateMaximumValueTests = [evalCrowdFunding, evalDepositIncentive, evalEscrow]
+  where
+    evalCrowdFunding = testCase "Evaluate CrowdFunding balances" $ do
+        let bounds = Map.fromList [(Right $ IdentChoice 1, 1000), (Right $ IdentChoice 2, 1000),
+                (Right $ IdentChoice 3, 1000), (Right $ IdentChoice 4, 1000)]
+        let balances = evaluateMaximumValue bounds crowdFunding
+        balances @?= Map.fromList [ (1, Balance 0 1000),
+                                    (2, Balance 0 1000),
+                                    (3, Balance 0 1000),
+                                    (4, Balance 0 1000),
+                                    (5, Balance 4000 0) ]
+
+    evalDepositIncentive = testCase "Evaluate DepositIncentive balances" $ do
+        let balances = evaluateMaximumValue Map.empty depositIncentive
+        balances @?= Map.fromList [ (1, Balance 20 0),
+                                    (2, Balance 0 20) ]
+
+    evalEscrow = testCase "Evaluate Escrow balances" $ do
+        let balances = evaluateMaximumValue Map.empty escrow
+        balances @?= Map.fromList [ (1, Balance 0 450),
+                                    (2, Balance 450 0) ]
+
+tests :: [TestTree]
+tests = evaluateMaximumValueTests
 
 main :: IO ()
 main = do
@@ -19,14 +47,4 @@ main = do
     let c = SPJ.One
     print $ translateSPJContractToMarlowe 1 8 (SPJ.One)
     print $ translateSPJContractToMarlowe 1 8 (SPJ.Give $ SPJ.One)
-    print $ evaluateMaximumValue Map.empty (
-        CommitCash (IdentCC 1) 1 (Value 123) 10 20
-            (Pay (IdentPay 1) 1 2 (Value 120) 5 Null)
-            (Pay (IdentPay 1) 1 2 (Value 122) 5 Null))
-
-
-    let bounds = Map.fromList [(Right $ IdentChoice 1, 1000), (Right $ IdentChoice 2, 1000),
-            (Right $ IdentChoice 3, 1000), (Right $ IdentChoice 4, 1000)]
-    print $ evaluateMaximumValue bounds crowdFunding
-    print $ evaluateMaximumValue bounds depositIncentive
-    print $ evaluateMaximumValue bounds escrow
+    defaultMain (testGroup "Marlowe Tests" tests)

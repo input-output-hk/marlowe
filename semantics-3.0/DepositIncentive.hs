55f6adc0c20c6fa6bd5e523e9c2b72c721ac1f00
Pablo Lamela <pablo.lamela@iohk.io>
2019-07-17 12:34:21 +0100

    Fix typo and add type declarations

--- semantics-3.0/DepositIncentive.hs
+++ semantics-3.0/DepositIncentive.hs
@@ -8,13 +8,13 @@ payAll acId payee cont =
 
 contract :: Contract
 contract =
-  When [ Case (Deposit incentivAcc incentiviser (incentiveAmt))
+  When [ Case (Deposit incentiveAcc incentiviser (incentiveAmt))
               (When [ Case (Deposit depositAcc depositer (depositAmt))
                            (When [ Case (Choice (ChoiceId 1 depositer) [(1,1)])
                                         RefundAll
                                  ]
                                  200
-                                 (payAll incentivAcc (Account depositAcc)
+                                 (payAll incentiveAcc (Account depositAcc)
                                          RefundAll))
                     ]
                     20
@@ -23,11 +23,13 @@ contract =
        10
        RefundAll
 
+depositAmt, incentiveAmt :: Value
 depositAmt = Constant 100
 incentiveAmt = Constant 20
 
+depositAcc, incentiveAcc :: AccountId 
 depositAcc = AccountId 1 depositer
-incentivAcc = AccountId 1 incentiviser
+incentiveAcc = AccountId 1 incentiviser
 
 depositer, incentiviser :: Party
 depositer = 1

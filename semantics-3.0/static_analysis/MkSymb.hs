c840c78dd2f822b0a76fb699deb199a35dfd274b
Pablo Lamela <pablo.lamela@iohk.io>
2019-08-05 15:14:22 +0100

    Implement unNest function generation

--- semantics-3.0/static_analysis/MkSymb.hs
+++ semantics-3.0/static_analysis/MkSymb.hs
@@ -39,6 +39,26 @@ mkNestFunc typeBaseName typeName typeNName clauses =
      declaration <- funD nfName $ nestFunClauses clauses id
      return [signature, declaration]
 
+unNestFunClauses :: [Con] -> (PatQ -> PatQ) -> [ClauseQ]
+unNestFunClauses [] f = error "No constructors for type"
+unNestFunClauses [NormalC name params] f =
+  [do names <- mapM (\x -> newName ('p':(show x))) [1..(length params)]
+      clause [f (tupP (map varP names))]
+             (normalB (foldl' appE (conE name) [ varE n | n <- names ])) []]
+unNestFunClauses list f = (unNestFunClauses fHalf (f . (\x -> [p| Left $(x) |]))) ++
+                          (unNestFunClauses sHalf (f . (\x -> [p| Right $(x) |])))
+  where ll = length list
+        (fHalf, sHalf) = splitAt (ll - (length list `div` 2)) list
+
+mkUnNestFunc :: String -> TH.Name -> TH.Name -> [Con] -> TH.Q [TH.Dec]
+mkUnNestFunc typeBaseName typeName typeNName clauses =
+  do let unNestFunName = ("unNest" ++ typeBaseName)
+     let nfName = mkName unNestFunName
+     signature <- sigD nfName (appT (appT arrowT (conT typeNName))
+                                         (conT typeName))
+     declaration <- funD nfName $ unNestFunClauses clauses id
+     return [signature, declaration]
+
 mkSymbolicDatatype :: TH.Name -> TH.Q [TH.Dec]
 mkSymbolicDatatype typeName =
   do TyConI (DataD _ _ _ _ clauses _) <- reify typeName
@@ -46,5 +66,6 @@ mkSymbolicDatatype typeName =
      let nName = mkName ('N':typeBaseName)
      nestedDecl <- mkNestedDec nName clauses
      nestFunc <- mkNestFunc typeBaseName typeName nName clauses
-     return (nestedDecl:nestFunc)
+     unNestFunc <- mkUnNestFunc typeBaseName typeName nName clauses
+     return (nestedDecl:(nestFunc ++ unNestFunc))
 

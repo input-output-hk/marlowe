6e30a246b63d26ee01745ae17e53a3708c3b497f
Pablo Lamela <pablo.lamela@iohk.io>
2019-08-29 13:07:58 +0100

    Implement mechanism to keep track of non-numeric identifiers

---
+++ semantics-3.0/src/Language/Marlowe/Analysis/Numbering.hs
@@ -0,0 +1,33 @@
+module Language.Marlowe.Analysis.Numbering (Numbering, emptyNumbering, getNumbering, getLabel) where
+
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+data Numbering a = Numbering { numbering :: (Map a Integer)
+                             , label :: (Map Integer a)
+                             , lastNumbering :: Integer }
+
+emptyNumbering :: Ord a => Numbering a
+emptyNumbering = Numbering { numbering = Map.empty
+                           , label = Map.empty
+                           , lastNumbering = 0 }
+
+getNewNumbering :: Ord a => Numbering a -> (Integer, Numbering a)
+getNewNumbering num@(Numbering {lastNumbering = x}) = (nx, num{lastNumbering = nx})
+  where nx = x + 1
+
+getNumbering :: Ord a => Numbering a -> a -> (Integer, Numbering a)
+getNumbering num x =
+  case Map.lookup x (numbering num) of
+    Just y -> (y, num)
+    Nothing -> let (y, newNum) = getNewNumbering num in
+               (y, newNum { numbering = Map.insert x y (numbering newNum)
+                          , label = Map.insert y x (label newNum) })
+
+getLabel :: Ord a => Numbering a -> Integer -> a
+getLabel num x =
+  case Map.lookup x (label num) of
+    Just y -> y
+    Nothing -> error "Numbering not defined"
+
+

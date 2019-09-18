192705cb088478ffe10c2914fb61d995e6ac9ceb
Pablo Lamela <pablo.lamela@iohk.io>
2019-09-19 00:17:51 +0100

    Remove "ValueId" from show and read instances

--- semantics-3.0/src/Language/Marlowe/Semantics.hs
+++ semantics-3.0/src/Language/Marlowe/Semantics.hs
@@ -78,6 +78,14 @@ module Language.Marlowe.Semantics where
       deriving stock (Eq,Ord,Show,Read,Generic)
       deriving anyclass Pretty
     
+    instance Pretty ValueId where
+      prettyFragment = text . show
+    
+    instance Show ValueId where
+      showsPrec p (ValueId txt) r = showsPrec p (T.unpack txt) r
+    instance Read ValueId where
+      readsPrec p x = [(ValueId (T.pack v), s) | (v, s) <- readsPrec p x]
+    
     data Value = AvailableMoney AccountId
                | Constant Integer
                | NegValue Value

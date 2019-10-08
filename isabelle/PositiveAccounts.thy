2629836c209a25eff4c56f7c668376a0e6c92579
Pablo Lamela <pablo.lamela@iohk.io>
2019-10-08 17:29:22 +0100

    Rewrite accountsArePositive

--- isabelle/PositiveAccounts.thy
+++ isabelle/PositiveAccounts.thy
@@ -2,15 +2,22 @@ theory PositiveAccounts
 imports Semantics
 begin
 
+theorem accountsArePositive :
+  "(\<forall>x. moneyInAccount x (accounts state) \<ge> 0) \<Longrightarrow>
+   computeTransaction txIn state cont = TransactionOutput txOut \<Longrightarrow>
+   moneyInAccount y (accounts (txOutState txOut)) \<ge> 0"
+  oops
+
+(* Alternative formulation *)
+
 fun allAccountsPositive :: "(AccountId \<times> Money) list \<Rightarrow> bool" where
-"allAccountsPositive accs = foldl (\<lambda> r (_, money) . r \<and> money > 0) True accs"
+"allAccountsPositive accs = foldl (\<lambda> r (_, money) . r \<and> money \<ge> 0) True accs"
 
 
 fun allAccountsPositiveState :: "State \<Rightarrow> bool" where
 "allAccountsPositiveState state = allAccountsPositive (accounts state)"
 
-
-lemma positiveMoneyInAccount [simp] :
+lemma positiveMoneyInAccount :
   "allAccountsPositive accs \<Longrightarrow> 0 \<le> moneyInAccount accId accs"
   oops
 
@@ -19,7 +26,7 @@ lemma addMoneyToAccountPositive :
   \<Longrightarrow> allAccountsPositive (addMoneyToAccount accId money accs)"
   oops
 
-theorem accountsArePositive :
+theorem accountsArePositive2 :
     "allAccountsPositiveState state
     \<Longrightarrow> computeTransaction txIn state cont = TransactionOutput txOut
     \<Longrightarrow> allAccountsPositiveState (txOutState txOut)"

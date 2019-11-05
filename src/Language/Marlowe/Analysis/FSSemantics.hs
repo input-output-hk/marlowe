4caf66bf879b2b3d98d64844b9ae933f265fc5c1
Pablo Lamela <pablo.lamela@iohk.io>
2019-11-05 16:44:57 +0000

    Restrict symbolic execution to non-negative initial slot numbers

--- src/Language/Marlowe/Analysis/FSSemantics.hs
+++ src/Language/Marlowe/Analysis/FSSemantics.hs
@@ -1119,6 +1119,6 @@ warningsTrace con =
         satCommand = proveWith (z3 {validateModel = True})
                                (forAll [snLabel, transListLabel]
                                    (\sn transList ->
-                                       (SL.length transList .<= literal maxActs) .=>
+                                       (SL.length transList .<= literal (maxActs + 1)) .&& (sn .>= literal 0) .=>
                                        SL.null (warningsFunc sn transList)))
 

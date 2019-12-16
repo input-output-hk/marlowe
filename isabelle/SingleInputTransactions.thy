95294697a918d62b7523e04d404cb6ea9d71c61b
Pablo Lamela <pablo.lamela@iohk.io>
2019-12-16 17:03:19 +0000

    Prove decomposition of applyAllInputs

--- isabelle/SingleInputTransactions.thy
+++ isabelle/SingleInputTransactions.thy
@@ -40,11 +40,77 @@ lemma reduceContractUntilQuiescentIdempotent :
   using reductionLoopIdempotent by blast  
 
 lemma applyAllLoopEmptyIdempotent :
-  "applyAllLoop env sta cont [] [] [] = ApplyAllSuccess wa pa nsta ncont \<Longrightarrow>
-   applyAllLoop env nsta ncont [] [] [] = ApplyAllSuccess [] [] nsta ncont"
+  "applyAllLoop env sta cont [] a b = ApplyAllSuccess wa pa nsta ncont \<Longrightarrow>
+   applyAllLoop env nsta ncont [] c d = ApplyAllSuccess c d nsta ncont"
   apply (simp only:applyAllLoop.simps[of env sta cont])
   apply (cases "reduceContractUntilQuiescent env sta cont")
   using reduceContractUntilQuiescentIdempotent apply auto[1]
   by simp
+
+lemma applyAllLoopJustAppendsWarningsAndEffects :
+  "applyAllLoop env st c list (wa @ wt) (ef @ et) = ApplyAllSuccess (wa @ nwa) (ef @ nef) fsta fcont \<Longrightarrow>
+   applyAllLoop env st c list (wa2 @ wt) (ef2 @ et) = ApplyAllSuccess (wa2 @ nwa) (ef2 @ nef) fsta fcont"
+  apply (induction list arbitrary: env st c wa wt ef et wa2 ef2 nwa nef)
+  subgoal for env st c wa wt ef et wa2 ef2 nwa nef
+    apply (simp only:applyAllLoop.simps[of env st c])
+    apply (cases "reduceContractUntilQuiescent env st c")
+    by simp_all
+  subgoal for head tail env st c wa wt ef et wa2 ef2 nwa nef
+    apply (simp only:applyAllLoop.simps[of env st c "(head # tail)"])
+    apply (cases "reduceContractUntilQuiescent env st c")
+    subgoal for tempWa tempEf tempState tempContract
+      apply (simp only:ReduceResult.case)
+      apply (subst list.case(2)[of _ _ head tail])
+      apply (subst (asm) list.case(2)[of _ _ head tail])
+      apply (cases "applyInput env tempState head tempContract")
+      apply (metis ApplyResult.simps(4) append.assoc)
+      by simp
+    by simp
+  done
+
+lemma applyLoopIdempotent_base_case :
+  "applyAllLoop env sta cont [] twa tef = ApplyAllSuccess wa pa nsta ncont \<Longrightarrow>
+   applyAllLoop env nsta ncont t [] [] = ApplyAllSuccess nwa npa fsta fcont \<Longrightarrow>
+   applyAllLoop env sta cont t twa tef = ApplyAllSuccess (wa @ nwa) (pa @ npa) fsta fcont"
+  apply (simp only:applyAllLoop.simps[of env sta cont])
+  apply (cases "reduceContractUntilQuiescent env sta cont")
+  apply (simp only:ReduceResult.case list.case)
+  apply (simp only:applyAllLoop.simps[of env nsta ncont])
+  apply (cases "reduceContractUntilQuiescent env nsta ncont")
+  apply (simp only:ReduceResult.case list.case)
+  apply (cases t)
+  apply (simp only:list.case)
+  using reduceContractUntilQuiescentIdempotent apply auto[1]
+  apply (simp only:list.case)
+  subgoal for x11 x12 x13 x14 x11a x12a x13a x14a a list
+    apply (cases "applyInput env x13a a x14a")
+    apply (cases "applyInput env x13 a x14")
+    apply (simp only:ApplyResult.case)
+    apply (smt ApplyAllResult.inject ApplyResult.inject ReduceResult.inject append.right_neutral append_assoc applyAllLoopJustAppendsWarningsAndEffects convertReduceWarnings.simps(1) reduceContractUntilQuiescentIdempotent self_append_conv2)
+    using reduceContractUntilQuiescentIdempotent apply auto[1]
+    by simp
+   apply simp
+  by simp
+
+lemma applyLoopIdempotent :
+  "applyAllLoop env sta cont [h] [] [] = ApplyAllSuccess wa pa nsta ncont \<Longrightarrow>
+   applyAllLoop env nsta ncont t [] [] = ApplyAllSuccess nwa npa fsta fcont \<Longrightarrow>
+   applyAllLoop env sta cont (h # t) [] [] = ApplyAllSuccess (wa @ nwa) (pa @ npa) fsta fcont"
+  apply (simp only:applyAllLoop.simps[of env sta cont])
+  apply (cases "reduceContractUntilQuiescent env sta cont")
+  apply (simp only:ReduceResult.case Let_def list.case)
+  subgoal for x11 x12 x13 x14
+    apply (cases "applyInput env x13 h x14")
+    subgoal for x11a x12a x13a
+      using applyLoopIdempotent_base_case by auto
+    by simp
+  by simp
+
+lemma applyAllIterative :
+  "applyAllInputs env sta cont [h] = ApplyAllSuccess wa pa nsta ncont \<Longrightarrow>
+   applyAllInputs env nsta ncont t = ApplyAllSuccess nwa npa fsta fcont \<Longrightarrow>
+   applyAllInputs env sta cont (h#t) = ApplyAllSuccess (wa @ nwa) (pa @ npa) fsta fcont"
+  apply (simp only:applyAllInputs.simps)
+  using applyLoopIdempotent by blast
 
 end

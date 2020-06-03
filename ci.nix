927d3161f2696bc76ebeec1eadc26f2066530a37
dk14 <dmkndr@gmail.com>
2020-06-03 16:06:02 +0700

    Update ci.nix

--- ci.nix
+++ ci.nix
@@ -2,6 +2,7 @@ builtins.mapAttrs (k: _v:
   let
     url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
     pkgs = import (builtins.fetchTarball url) { system = k; };
+    
     isabelleNix = import ./isabelle.nix; 
   in
   pkgs.recurseIntoAttrs {

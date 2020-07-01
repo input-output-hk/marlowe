67976a287f62738c52cd33ac1e43fbc7bec56a3b
dk14 <dmkndr@gmail.com>
2020-07-02 03:53:25 +0700

    fix

--- ci.nix
+++ ci.nix
@@ -2,18 +2,12 @@ builtins.mapAttrs (k: _v:
   let
     url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
     pkgs = import (builtins.fetchTarball url) { system = k; };
-    haskellNix = import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {};
-    haskellNixPkgs = import haskellNix.sources.nixpkgs-2003 haskellNix.nixpkgsArgs;
     isabelleNix = import ./isabelle.nix; 
+    haskellNix = import ./haskell.nix; 
   in
   pkgs.recurseIntoAttrs {
     testIsabelle = isabelleNix.isabelleTest;
-    testHaskell = haskellNixPkgs.haskell-nix.project {
-      src = haskellNixPkgs.haskell-nix.haskellLib.cleanGit {
-        name = "marlowe";
-        src = ./.;
-      };
-    };
+    testHaskell = haskellNix.marlowe.components.library
   }
 ) {
   x86_64-linux = {};

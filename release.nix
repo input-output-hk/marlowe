ca6bcfa2e92b192f67acfd0363b12585e6130932
Shea Levy <shea@shealevy.com>
2022-04-20 07:34:58 -0400

    Fix CI reporting

--- release.nix
+++ release.nix
@@ -1,14 +1,16 @@
-builtins.mapAttrs (k: _v:
-  let
-    url = "https://github.com/NixOS/nixpkgs/archive/4358b0fd675c6a0e07140cd85caf1bc18ca80183.tar.gz";
-    pkgs = import (builtins.fetchTarball url) { system = k; };
-    isabelleNix = import ./isabelle.nix; 
-    haskellNix = import ./haskell.nix {}; 
-  in
-  pkgs.recurseIntoAttrs {
+let
+  url = "https://github.com/NixOS/nixpkgs/archive/ec6eaba9dfcfdd11547d75a193e91e26701bf7e3.tar.gz";
+  pkgs = import (builtins.fetchTarball url) { system = "x86_64-linux"; };
+  isabelleNix = import ./isabelle.nix;
+  haskellNix = import ./haskell.nix {};
+  jobs = {
     testIsabelle = isabelleNix.isabelleTest;
     testHaskell = haskellNix.marlowe.components.exes.marlowe ;
-  }
-) {
-  x86_64-linux = {};
+  };
+in {
+  x86_64-linux = jobs;
+  required = pkgs.releaseTools.aggregate {
+    name = "marlowe";
+    constituents = map (x: "x86_64-linux.${x}") (builtins.attrNames jobs);
+  };
 }

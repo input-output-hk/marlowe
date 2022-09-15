caa55320caed73d378752112e93ec22931f22e80
Oleg Grenrus <oleg@well-typed.com>
2022-09-15 15:13:07 +0300

    Add Bi/Traversable instances to parametric types

--- src/Language/Marlowe/Semantics/Types.hs
+++ src/Language/Marlowe/Semantics/Types.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Language.Marlowe.Semantics.Types where
@@ -9,6 +10,9 @@ import           GHC.Generics (Generic)
 import           Data.Text (Text)
 import           Language.Marlowe.Pretty (Pretty, prettyFragment)
 import           Data.ByteString (ByteString)
+import           Data.Bifoldable (Bifoldable (bifoldMap))
+import           Data.Bifunctor (Bifunctor (bimap))
+import           Data.Bitraversable (Bitraversable (bitraverse), bimapDefault, bifoldMapDefault)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Text.PrettyPrint.Leijen (text)
@@ -32,7 +36,7 @@ instance Num POSIXTime where
 
 data Party i = PubKey i
              | Role ByteString
-  deriving (Eq,Ord,Show,Read,Generic,Pretty)
+  deriving (Eq,Ord,Show,Read,Generic,Pretty,Functor,Foldable,Traversable)
 
 type AccountId = Party
 type ChoiceName = ByteString     -- Needs to be updated in playground.
@@ -43,7 +47,7 @@ type ChosenNum = Integer
 type Accounts i t = Map (AccountId i, t) Integer
 
 data ChoiceId i = ChoiceId ChoiceName (Party i)
-  deriving (Eq,Ord,Show,Read,Generic,Pretty)
+  deriving (Eq,Ord,Show,Read,Generic,Pretty,Functor,Foldable,Traversable)
 
 newtype ValueId = ValueId Text
   deriving stock (Eq,Ord,Generic)
@@ -71,7 +75,23 @@ data Value i t = AvailableMoney (Party i) t
                | TimeIntervalEnd
                | UseValue ValueId
                | Cond (Observation i t) (Value i t) (Value i t)
-  deriving (Eq,Ord,Show,Read,Generic,Pretty)
+  deriving (Eq,Ord,Show,Read,Generic,Pretty,Functor,Foldable,Traversable)
+
+instance Bifunctor  Value where bimap = bimapDefault
+instance Bifoldable Value where bifoldMap = bifoldMapDefault
+instance Bitraversable Value where
+    bitraverse f g (AvailableMoney a b) = AvailableMoney <$> traverse f a <*> g b
+    bitraverse _ _ (Constant x)         = pure (Constant x)
+    bitraverse f g (NegValue a)         = NegValue <$> bitraverse f g a
+    bitraverse f g (AddValue a b)       = AddValue <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (SubValue a b)       = SubValue <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (MulValue a b)       = MulValue <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (DivValue a b)       = DivValue <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f _ (ChoiceValue a)      = ChoiceValue <$> traverse f a
+    bitraverse _ _ TimeIntervalStart    = pure TimeIntervalStart
+    bitraverse _ _ TimeIntervalEnd      = pure TimeIntervalEnd
+    bitraverse _ _ (UseValue a)         = pure (UseValue a)
+    bitraverse f g (Cond a b c)         = Cond <$> bitraverse f g a <*> bitraverse f g b <*> bitraverse f g c
 
 data Observation i t = AndObs (Observation i t) (Observation i t)
                      | OrObs (Observation i t) (Observation i t)
@@ -84,7 +104,22 @@ data Observation i t = AndObs (Observation i t) (Observation i t)
                      | ValueEQ (Value i t) (Value i t)
                      | TrueObs
                      | FalseObs
-  deriving (Eq,Ord,Show,Read,Generic,Pretty)
+  deriving (Eq,Ord,Show,Read,Generic,Pretty,Functor,Foldable,Traversable)
+
+instance Bifunctor  Observation where bimap = bimapDefault
+instance Bifoldable Observation where bifoldMap = bifoldMapDefault
+instance Bitraversable Observation where
+    bitraverse f g (AndObs a b)       = AndObs <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (OrObs a b)        = OrObs <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (NotObs a)         = NotObs <$> bitraverse f g a
+    bitraverse f _ (ChoseSomething a) = ChoseSomething <$> traverse f a
+    bitraverse f g (ValueGE a b)      = ValueGE <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (ValueGT a b)      = ValueGT <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (ValueLT a b)      = ValueLT <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (ValueLE a b)      = ValueLE <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (ValueEQ a b)      = ValueEQ <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse _ _ TrueObs            = pure TrueObs
+    bitraverse _ _ FalseObs           = pure FalseObs
 
 data TimeInterval = TimeInterval POSIXTime POSIXTime
   deriving (Eq,Ord,Show,Read,Generic,Pretty)
@@ -103,15 +138,28 @@ inBounds num = any (\(Bound l u) -> num >= l && num <= u)
 data Action i t = Deposit (Party i) (Party i) t (Value i t)
                 | Choice (ChoiceId i) [Bound]
                 | Notify (Observation i t)
-  deriving (Eq,Ord,Show,Read,Generic,Pretty)
+  deriving (Eq,Ord,Show,Read,Generic,Pretty,Functor,Foldable,Traversable)
+
+instance Bifunctor  Action where bimap = bimapDefault
+instance Bifoldable Action where bifoldMap = bifoldMapDefault
+instance Bitraversable Action where
+    bitraverse f g (Deposit a b c d) = Deposit <$> traverse f a <*> traverse f b <*> g c <*> bitraverse f g d
+    bitraverse f _ (Choice a b)      = Choice <$> traverse f a <*> pure b
+    bitraverse f g (Notify a)        = Notify <$> bitraverse f g a
 
 data Payee i = Account (Party i)
              | Party (Party i)
-  deriving (Eq,Ord,Show,Read,Generic,Pretty)
+  deriving (Eq,Ord,Show,Read,Generic,Pretty,Functor,Foldable,Traversable)
 
 data Case i t = Case (Action i t) (Contract i t)
               | MerkleizedCase (Action i t) ByteString
-  deriving (Eq,Ord,Show,Read,Generic,Pretty)
+  deriving (Eq,Ord,Show,Read,Generic,Pretty,Functor,Foldable,Traversable)
+
+instance Bifunctor  Case where bimap = bimapDefault
+instance Bifoldable Case where bifoldMap = bifoldMapDefault
+instance Bitraversable Case where
+    bitraverse f g (Case   a b)         = Case <$> bitraverse f g a <*> bitraverse f g b
+    bitraverse f g (MerkleizedCase a b) = MerkleizedCase <$> bitraverse f g a <*> pure b
 
 getAction :: Case i t -> Action i t
 getAction (Case action _) = action
@@ -123,7 +171,17 @@ data Contract i t = Close
                   | When [Case i t] Timeout (Contract i t)
                   | Let ValueId (Value i t) (Contract i t)
                   | Assert (Observation i t) (Contract i t)
-  deriving (Eq,Ord,Show,Read,Generic,Pretty)
+  deriving (Eq,Ord,Show,Read,Generic,Pretty,Functor,Foldable,Traversable)
+
+instance Bifunctor  Contract where bimap = bimapDefault
+instance Bifoldable Contract where bifoldMap = bifoldMapDefault
+instance Bitraversable Contract where
+    bitraverse _ _ Close           = pure Close
+    bitraverse f g (Pay a b c d e) = Pay <$> traverse f a <*> traverse f b <*> g c <*> bitraverse f g d <*> bitraverse f g e
+    bitraverse f g (If a b c)      = If <$> bitraverse f g a <*> bitraverse f g b <*> bitraverse f g c
+    bitraverse f g (When a b c)    = When <$> traverse (bitraverse f g) a <*> pure b <*> bitraverse f g c
+    bitraverse f g (Let a b c)     = Let a <$> bitraverse f g b <*> bitraverse f g c
+    bitraverse f g (Assert a b)    = Assert <$> bitraverse f g a <*> bitraverse f g b
 
 data State i t = State { accounts    :: Map (Party i, t) Money
                        , choices     :: Map (ChoiceId i) ChosenNum
@@ -143,11 +201,24 @@ newtype Environment = Environment { timeInterval :: TimeInterval }
 data InputContent i t = IDeposit (Party i) (Party i) t Money
                       | IChoice (ChoiceId i) ChosenNum
                       | INotify
-  deriving (Eq,Ord,Show,Read)
+  deriving (Eq,Ord,Show,Read,Functor,Foldable,Traversable)
+
+instance Bifunctor  InputContent where bimap = bimapDefault
+instance Bifoldable InputContent where bifoldMap = bifoldMapDefault
+instance Bitraversable InputContent where
+    bitraverse f g (IDeposit a b c d) = IDeposit <$> traverse f a <*> traverse f b <*> g c <*> pure d
+    bitraverse f _ (IChoice a b)      = IChoice <$> traverse f a <*> pure b
+    bitraverse _ _ INotify            = pure INotify
 
 data Input i t = NormalInput (InputContent i t)
                | MerkleizedInput (InputContent i t) ByteString
-  deriving (Eq,Ord,Show,Read)
+  deriving (Eq,Ord,Show,Read,Functor,Foldable,Traversable)
+
+instance Bifunctor  Input where bimap = bimapDefault
+instance Bifoldable Input where bifoldMap = bifoldMapDefault
+instance Bitraversable Input where
+    bitraverse f g (NormalInput a)       = NormalInput <$> bitraverse f g a
+    bitraverse f g (MerkleizedInput a b) = MerkleizedInput <$> bitraverse f g a <*> pure b
 
 getInputContent :: Input i t -> InputContent i t
 getInputContent (NormalInput inputContent) = inputContent

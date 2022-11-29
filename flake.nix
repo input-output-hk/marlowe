9c67dff0c82d56203bb9cc894ec3b3b31b532762
Hernan Rajchert <hrajchert@gmail.com>
2022-11-29 12:35:33 -0300

    Add nix formatter and hooks

--- flake.nix
+++ flake.nix
@@ -13,10 +13,13 @@
     tullia.url = "github:input-output-hk/tullia";
 
     nixLsp.url = "github:oxalica/nil";
+
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+
   };
 
 
-  outputs = { self, flake-utils, nixpkgs, haskellNix, isabelle-nixpkgs, tullia, nixLsp }:
+  outputs = { self, flake-utils, nixpkgs, haskellNix, isabelle-nixpkgs, tullia, pre-commit-hooks, nixLsp }:
     let
       inherit (flake-utils.lib) eachSystem system;
 
@@ -27,7 +30,7 @@
         let
           evalSystem = evalSystem';
 
-          overlays = [ haskellNix.overlay nixLsp.overlays.nil];
+          overlays = [ haskellNix.overlay nixLsp.overlays.nil ];
 
           pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
 
@@ -38,43 +41,13 @@
             ${script}
           '';
           build-marlowe-proofs = writeShellScriptBinInRepoRoot "build-marlowe-proofs" ''
-            #!/bin/bash
             echo "Building Marlowe proofs"
 
-            # We build hold library (with -b) so that is available in
-            # the users HEAP directory for fast rebuilds. In a user
-            # machine it only builds the first time, the next time it
-            # will see that there are no changes.
+          scripts = import ./nix/scripts.nix {inherit pkgs writeShellScriptBinInRepoRoot;};
 
-            if [ "$1" != "false" ]; then
-              echo "Building HOL-Library"
-              isabelle build -v -b HOL-Library
-            fi
-
-            # We clean the generated files to avoid orphans
-            rm -Rf isabelle/generated
-
-            # We build the different sessions that conform the Marlowe specification
-            isabelle build -v -b -d isabelle Util
-            isabelle build -v -b -d isabelle Core
-            isabelle build -v -b -d isabelle Examples
-            isabelle build -v -b -e -d isabelle CodeExports
-            isabelle build -v -b -d isabelle StaticAnalysis
-          '';
-
-          build-marlowe-docs = writeShellScriptBinInRepoRoot "build-marlowe-docs" ''
-            echo "Generating Literate Haskell Tex"
-            lhs2TeX isabelle/haskell/MarloweCoreJson.lhs | sed '1,/PATTERN FOR SED/d' > isabelle/Doc/Specification/document/marlowe-core-json.tex
-
-            echo "Building Marlowe docs"
-            isabelle document -v -V -d isabelle -P papers Cheatsheet
-            isabelle document -v -V -d isabelle -P papers Specification
-          '';
-
-          edit-marlowe-proofs = writeShellScriptBinInRepoRoot "edit-marlowe-proofs" ''
-            cd isabelle
-            isabelle jedit -d . -u Doc/Specification/Specification.thy
-          '';
+          formatting = import ./nix/formatting.nix {
+            inherit writeShellScriptBinInRepoRoot pkgs;
+          };
 
           latex = (pkgs.texlive.combine {
             inherit (pkgs.texlive) scheme-basic ulem collection-fontsrecommended mathpartir stmaryrd polytable lazylist;
@@ -88,22 +61,39 @@
             inherit evalSystem;
             shell.tools.cabal = { inherit evalSystem; };
             shell.tools.haskell-language-server = { inherit evalSystem; };
+            shell.shellHook = ''
+              ${pre-commit-check.shellHook}
+
+              # The green prompt familiar to those used to nix-shell
+              export PS1="\n\[\033[1;32m\][nix develop:\w]\$\[\033[0m\] "
+            '';
             shell.inputsFrom = [ packages.isabelle-test ];
             shell.nativeBuildInputs =
-              ( with scripts; [
-                  build-marlowe-proofs
-                  edit-marlowe-proofs
-                  build-marlowe-docs
+              (with scripts; [
+                build-marlowe-proofs
+                edit-marlowe-proofs
+                build-marlowe-docs
               ]
               ) ++
-              [ latex pkgs.haskellPackages.lhs2tex tulliaPackage pkgs.nil];
+              (with pkgs; [
+                nil
+                haskellPackages.lhs2tex
+              ]) ++
+              [ latex tulliaPackage formatting.fix-nix-fmt ];
           };
 
           flake = project.flake { };
 
           tests = import ./nix/tests {
             src = ./.;
-            inherit pkgs  isabelle-pkgs scripts latex;
+            inherit pkgs isabelle-pkgs scripts latex;
+          };
+
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = (pkgs.lib.cleanSource ./.);
+            hooks = {
+              nixpkgs-fmt.enable = true;
+            };
           };
           packages = flake.packages // {
             inherit (tests) isabelle-test;

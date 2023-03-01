d4c4c993ba6f66d9b88aa82e0cddf5827c714fe8
Yves Hauser <yves.hauser@gmail.com>
2023-03-01 15:54:12 +0100

    SCP-4908: Added Escrow contract

--- isabelle/Examples/Escrow.thy
+++ isabelle/Examples/Escrow.thy
@@ -10,6 +10,7 @@ subsection \<open>Contract definition\<close>
 
 record EscrowArgs =
   price             :: Value
+  token             :: Token
   seller            :: Party
   buyer             :: Party
   mediator          :: Party
@@ -18,13 +19,13 @@ record EscrowArgs =
   disputeDeadline   :: Timeout
   mediationDeadline :: Timeout
 
-definition "ada = Token (BS '''') (BS '''')"
+definition "adaToken = Token (BS '''') (BS '''')"
 
 fun mediation :: "EscrowArgs \<Rightarrow> Contract" where
   "mediation args =
     When
       [ Case (Choice (ChoiceId (BS ''Dismiss claim'') (mediator args)) [Bound 0 0])
-          (Pay (buyer args) (Account (seller args)) ada (price args) Close)
+          (Pay (buyer args) (Account (seller args)) adaToken (price args) Close)
       , Case (Choice (ChoiceId (BS ''Confirm claim'') (mediator args)) [Bound 1 1]) Close
       ] (mediationDeadline args) Close"
 
@@ -40,24 +41,203 @@ fun report :: "EscrowArgs \<Rightarrow> Contract" where
     When
       [ Case (Choice (ChoiceId (BS ''Everything is alright'') (buyer args)) [Bound 0 0]) Close
       , Case (Choice (ChoiceId (BS ''Report problem'') (buyer args)) [Bound 1 1])
-          (Pay (seller args) (Account (buyer args)) ada (price args) (dispute args))
+          (Pay (seller args) (Account (buyer args)) adaToken (price args) (dispute args))
       ] (complaintDeadline args) Close"
 
 fun escrow :: "EscrowArgs \<Rightarrow> Contract" where
   "escrow args =
     When
-      [ Case (Deposit (seller args) (buyer args) ada (price args)) (report args) ]
+      [ Case (Deposit (seller args) (buyer args) adaToken (price args)) (report args) ]
       (paymentDeadline args) Close"
 
-definition "escrow_example = escrow
-  \<lparr> price = Constant 1
+definition "escrowArgs =
+  \<lparr> price = Constant 10
+  , token = adaToken
   , seller = Role (BS ''Seller'')
   , buyer = Role (BS ''Buyer'')
   , mediator = Role (BS ''Mediator'')
   , paymentDeadline = 1664812800000
-  , complaintDeadline = 1664812800000
-  , disputeDeadline = 1664812800000
-  , mediationDeadline = 1664812800000
+  , complaintDeadline = 1664816400000
+  , disputeDeadline = 1664820420000
+  , mediationDeadline = 1664824440000
   \<rparr>"
 
-value escrow_example
+definition "escrowExample = escrow escrowArgs"
+
+subsection \<open>Everything is alright.\<close>
+
+definition
+  "everythingIsAlrightTransactions =
+    [
+      \<comment> \<open>First party deposit\<close>
+      \<lparr> interval = (1664812600000, 1664812700000)
+      , inputs = [
+                  IDeposit
+                    (seller escrowArgs)
+                    (buyer escrowArgs)
+                    (token escrowArgs)
+                    10
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664812900000, 1664813100000)
+      , inputs = [ IChoice (ChoiceId (BS ''Everything is alright'') (buyer escrowArgs)) 0
+                 ]
+      \<rparr>
+    ]
+  "
+
+definition
+  "everythingIsAlrightPayments =
+    [ Payment (seller escrowArgs) (Party (seller escrowArgs)) (token escrowArgs) 10
+    ]
+  "
+
+proposition
+ "playTrace 0 escrowExample everythingIsAlrightTransactions = TransactionOutput txOut
+  \<Longrightarrow>
+     txOutContract txOut = Close
+     \<and> txOutPayments txOut = everythingIsAlrightPayments
+     \<and> txOutWarnings txOut = []"
+(*<*)apply (code_simp)
+     apply (auto simp add: everythingIsAlrightPayments_def escrowArgs_def adaToken_def)
+     done(*>*)
+
+subsection \<open>Confirm problem.\<close>
+
+definition
+  "confirmProblemTransactions =
+    [
+      \<comment> \<open>First party deposit\<close>
+      \<lparr> interval = (1664812600000, 1664812700000)
+      , inputs = [
+                  IDeposit
+                    (seller escrowArgs)
+                    (buyer escrowArgs)
+                    (token escrowArgs)
+                    10
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664812900000, 1664813100000)
+      , inputs = [ IChoice (ChoiceId (BS ''Report problem'') (buyer escrowArgs)) 1
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664817400000, 1664817400000)
+      , inputs = [ IChoice (ChoiceId (BS ''Confirm problem'') (seller escrowArgs)) 1
+                 ]
+      \<rparr>
+    ]
+  "
+
+definition
+  "confirmProblemPayments =
+    [ Payment (seller escrowArgs) (Account (buyer escrowArgs)) (token escrowArgs) 10
+    , Payment (buyer escrowArgs) (Party (buyer escrowArgs)) (token escrowArgs) 10
+    ]
+  "
+
+proposition
+ "playTrace 0 escrowExample confirmProblemTransactions = TransactionOutput txOut
+  \<Longrightarrow>
+     txOutContract txOut = Close
+     \<and> txOutPayments txOut = confirmProblemPayments
+     \<and> txOutWarnings txOut = []"
+(*<*)apply (code_simp)
+     apply (auto simp add: confirmProblemPayments_def escrowArgs_def adaToken_def)
+     done(*>*)
+
+subsection \<open>Dismiss claim.\<close>
+
+definition
+  "dismissClaimTransactions =
+    [
+      \<comment> \<open>First party deposit\<close>
+      \<lparr> interval = (1664812600000, 1664812700000)
+      , inputs = [
+                  IDeposit
+                    (seller escrowArgs)
+                    (buyer escrowArgs)
+                    (token escrowArgs)
+                    10
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664812900000, 1664813100000)
+      , inputs = [ IChoice (ChoiceId (BS ''Report problem'') (buyer escrowArgs)) 1
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664817400000, 1664817400000)
+      , inputs = [ IChoice (ChoiceId (BS ''Dispute problem'') (seller escrowArgs)) 0
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664821400000, 1664822400000)
+      , inputs = [ IChoice (ChoiceId (BS ''Dismiss claim'') (mediator escrowArgs)) 0
+                 ]
+      \<rparr>
+    ]
+  "
+
+definition
+  "dismissClaimPayments =
+    [ Payment (seller escrowArgs) (Account (buyer escrowArgs)) (token escrowArgs) 10
+    , Payment (buyer escrowArgs) (Account (seller escrowArgs)) (token escrowArgs) 10
+    , Payment (seller escrowArgs) (Party (seller escrowArgs)) (token escrowArgs) 10
+    ]
+  "
+
+proposition
+ "playTrace 0 escrowExample dismissClaimTransactions = TransactionOutput txOut
+  \<Longrightarrow>
+     txOutContract txOut = Close
+     \<and> txOutPayments txOut = dismissClaimPayments
+     \<and> txOutWarnings txOut = []"
+(*<*)apply (code_simp)
+     apply (auto simp add: dismissClaimPayments_def escrowArgs_def adaToken_def)
+     done(*>*)
+
+subsection \<open>Confirm claim.\<close>
+
+definition
+  "confirmClaimTransactions =
+    [
+      \<comment> \<open>First party deposit\<close>
+      \<lparr> interval = (1664812600000, 1664812700000)
+      , inputs = [
+                  IDeposit
+                    (seller escrowArgs)
+                    (buyer escrowArgs)
+                    (token escrowArgs)
+                    10
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664812900000, 1664813100000)
+      , inputs = [ IChoice (ChoiceId (BS ''Report problem'') (buyer escrowArgs)) 1
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664817400000, 1664817400000)
+      , inputs = [ IChoice (ChoiceId (BS ''Dispute problem'') (seller escrowArgs)) 0
+                 ]
+      \<rparr>
+   ,  \<lparr> interval = (1664821400000, 1664822400000)
+      , inputs = [ IChoice (ChoiceId (BS ''Confirm claim'') (mediator escrowArgs)) 1
+                 ]
+      \<rparr>
+    ]
+  "
+
+definition
+  "confirmClaimPayments =
+    [ Payment (seller escrowArgs) (Account (buyer escrowArgs)) (token escrowArgs) 10
+    , Payment (buyer escrowArgs) (Party (buyer escrowArgs)) (token escrowArgs) 10
+    ]
+  "
+
+proposition
+ "playTrace 0 escrowExample confirmClaimTransactions = TransactionOutput txOut
+  \<Longrightarrow>
+     txOutContract txOut = Close
+     \<and> txOutPayments txOut = confirmClaimPayments
+     \<and> txOutWarnings txOut = []"
+(*<*)apply (code_simp)
+     apply (auto simp add: confirmClaimPayments_def escrowArgs_def adaToken_def)
+     done(*>*)
+
+end

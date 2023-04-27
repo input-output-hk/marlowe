0227f3b2a94dd4d27a4b3e74e607b25c064a4c8a
Hernan Rajchert <hrajchert@gmail.com>
2023-04-27 17:20:56 -0300

    Refactor guarantees chapter

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@
 - [SCP-4869](https://github.com/input-output-hk/marlowe/pull/164) - Add lemma for DivValue rounding to zero
 - [SCP-4908](https://github.com/input-output-hk/marlowe/pull/163) - Add Escrow to the contract examples
 - [PLT-3761](https://github.com/input-output-hk/marlowe/pull/182) - Refactor semantic theory into literate programming
+- [PLT-5263](https://github.com/input-output-hk/marlowe/pull/186) - Refactor guarantees section
+
 ## Test Spec
 - [SCP-4860](https://github.com/input-output-hk/marlowe/pull/158) - Make tests reproducible
 - [SCP-4698](https://github.com/input-output-hk/marlowe/pull/154) - Implement roundtrip test for all serialized types
--- isabelle/Doc/Specification/Guarantees.thy
+++ isabelle/Doc/Specification/Guarantees.thy
@@ -14,9 +14,94 @@ begin
 
 chapter \<open>Marlowe Guarantees \label{sec:marlowe-guarantees} \<close>
 
-text \<open>We can also use the Isabelle proof assistant to prove that the Marlowe semantics presents certain
-desirable properties, such as that assets are preserved and anything unspent is returned to users by
-the end of the execution of any contract.\<close>
+text \<open>We can also use the Isabelle proof assistant to prove that the Marlowe semantics
+ presents certain desirable properties, such as that assets are preserved and anything
+ unspent is returned to users by the end of the execution of any contract.\<close>
+
+text \<open>To keep this section concise, whenever we introduce a helper function we'll 
+provide a brief explanation and the type signature. We'll also add the name of the 
+theories and files to access the full definitions.\<close>
+
+section \<open>Positive Accounts\<close>
+
+text \<open>The Marlowe State represents the \<^emph>\<open>accounts\<close>, \<^emph>\<open>choices\<close> and \<^emph>\<open>boundValues\<close> Maps as
+associative lists \secref{sec:state}. While we make proofs in Isabelle, we often need to
+ assert that two Maps are equal, and with the list representation, this means that the 
+maps should be in the same order and without duplicate keys. To ensure this, we have the following helper located 
+in the MList.thy theory\<close>
+
+text \<open>
+\<^item> valid\_map :: @{typeof valid_map}
+\<close>
+
+text \<open>Moreover, we use the following function to ensure that the Maps are valid and the
+ \<^emph>\<open>accounts\<close> only have positive values\<close>
+
+text \<open>
+\<^item> validAndPositive\_state :: @{typeof validAndPositive_state}
+\<close>
+
+text \<open>
+The following theorem asserts that if the State is valid before computing a transaction,
+it is also valid after.
+\<close>
+
+text \<open>\<^bold>\<open>lemma\<close> \<^emph>\<open>computeTransaction\_preserves\_validAndPositive\_state\<close>:@{thm [display,names_short, margin=40] computeTransaction_preserves_validAndPositive_state}\<close>
+
+text \<open>The function and lemma are located in \<^emph>\<open>PositiveAccounts.thy\<close>.\<close>
+
+section \<open>Finite contracts\label{sec:finite-contracts}\<close>
+
+text \<open>
+All contracts are finite and will end at a particular time. We can use the following function
+(located in Semantics.thy) to see what that time is\<close>
+
+(* We use a hardcoded type as aliases get expanded *)
+text \<open>
+\<^item> maxTimeContract :: \isa{Contract\ {\isasymRightarrow}\ POSIXTime}\<close>
+
+text 
+\<open>
+The Blockchain doesn't have the notion of a cronjob, so it can't refund participants
+after the contract timeouts on its own. Instead we can guarantee that computing a transaction
+without inputs with a validity interval that starts after the max time will always succeed 
+\<close>
+
+text \<open>\<^bold>\<open>corollary\<close> \<^emph>\<open>timeOutTransaction\_does\_not\_fail\<close>: @{thm [display,names_short, margin=60] timeOutTransaction_does_not_fail}\<close>
+
+text \<open>Moreover, we can use the function \<^emph>\<open>isClosedAndEmpty\<close> to
+assert that the previous transaction will result in a Close contract with empty accounts.\<close>
+
+text \<open>
+\<^item> isClosedAndEmpty :: @{typeof isClosedAndEmpty}\<close>
+
+text \<open>\<^bold>\<open>theorem\<close> \<^emph>\<open>timedOutTransaction\_closes\_contract\<close>:@{thm [display,names_short, margin=60] timedOutTransaction_closes_contract}\<close>
+
+text \<open>The helper and lemmas are located in \<^emph>\<open>Timeout.thy\<close>.\<close>
+
+
+subsection \<open>Transaction Bound\label{sec:transaction-bound}\<close>
+
+text \<open>There is a maximum number of transactions that can be accepted by a contract.\<close>
+
+text \<open>
+\<^item> maxTransactionsInitialState :: @{typeof maxTransactionsInitialState}
+\<close>
+
+text \<open>We can prove that if we run a successful trace, the number of transactions won't
+exceed this value\<close>
+
+(* should we have a maxTransactions :: Contract \<Rightarrow> Int in the semantics? *)
+
+text \<open>\<^bold>\<open>lemma\<close> \<^emph>\<open>playTrace\_only\_accepts\_maxTransactionsInitialState\<close>: @{thm  [display,names_short, margin=60] playTrace_only_accepts_maxTransactionsInitialState}\<close>
+
+text \<open>The helper and lemma are located in \<^emph>\<open>TransactionBound.thy\<close>.\<close>
+
+subsection "Close is safe"
+text \<open>Computing a transaction on a Closed contract doesn't produce any warnings \<close>
+text \<open>\<^bold>\<open>theorem\<close> \<^emph>\<open>closeIsSafe\<close>: @{thm closeIsSafe}\<close>
+
+text \<open>This theorem is located in \<^emph>\<open>CloseSafe.thy\<close>.\<close>
 
 section \<open>Assets Preservation\<close>
 
@@ -24,122 +109,92 @@ text \<open>
 
 One of the dangers of using smart contracts is that a badly written one can potentially lock its
 funds forever.\<close>
+
 text \<open>By the end of a Marlowe contract, all the assets paid to the contract must be returned to a subset of the participants. To ensure this behaviour we
-proved two properties: ``MultiAssets Preservation'' and ``Contracts Always Close''.\<close>
+proved two properties: ``MultiAssets Preservation'' and ``Refund after timeout''.\<close>
+
+subsection "Multi assets preservation"
+
+text \<open>To express this theory we need the following helper functions\<close>
 
 text \<open>
-Regarding asset preservation, tokens are not created nor destroyed by the semantics. Formally speaking,
+\<^item> assetsInExternalPayments :: @{typeof assetsInExternalPayments}
+\<^item> assetsInTransaction :: @{typeof assetsInTransaction}
+\<^item> assetsInState :: @{typeof assetsInState}
+\<close>
+
+text \<open>These functions extracts the \<^emph>\<open>amount\<close> of \<^emph>\<open>token\<close> available in different 
+structures. We store this information in a new type called \<^emph>\<open>Assets\<close>, that allow us 
+to do addition and subtraction keeping the token information \<^footnote>\<open>The Assets type is located
+in MultiAssets.thy, the functions and theorem are in AssetPreservation.thy\<close>.\<close>
+
+text \<open>
+The following theorem proves that tokens are not created nor destroyed by the semantics. Formally speaking,
 if a transaction is computed successfully, the sum of the assets stored in the previous state and
 the assets deposited by a transaction are equal to the assets in the new state and possible external payments
 (from internal account to external party).
 \<close>
 
-text \<open>@{thm computeTransaction_preserves_assets}\<close>
 
-text \<open>If calling computeTransaction yields an error, then the transaction is invalid and should not be
+text \<open>\<^bold>\<open>theorem\<close> \<^emph>\<open>computeTransaction\_preserves\_assets\<close>: @{thm [display,names_short, margin=40] computeTransaction_preserves_assets}\<close>
+
+text \<open>If calling \<^emph>\<open>computeTransaction\<close> yields an error, then the transaction is invalid and should not be
 part of the blockchain, preserving assets in its current state.\<close>
 
-section \<open>Contracts Always Close\<close>
+subsection \<open>Refund after timeout\<close>
+
+text \<open>
+In section \secref{sec:finite-contracts} we proved that there is a maximum time that 
+allows any participant to create an empty transaction that will close the contract 
+successfully.
+\<close>
+
+text \<open>The following theorem proves that the transaction also preserves assets\<close>
+
+text \<open>\<^bold>\<open>theorem\<close> \<^emph>\<open>timedOutTransaction\_preserves\_assets\<close>@{thm [display,names_short, margin=60] timedOutTransaction_preserves_assets}\<close>
+
+text \<open>This theorem can be found on the \<^emph>\<open>Timeout.thy\<close> theory.\<close>
+
+section \<open>Quiescent\<close>
 
 text \<open>
 
-For every Marlowe Contract there is a maximum time that allows any participant
-to create an empty transaction that will close the contract successfully.
-\<close>
-
-text \<open>@{thm [display,names_short, margin=40] timeOutTransaction_does_not_fail}\<close>
-
-text \<open>Moreover, that transaction empties the account and produces payments for any lingering assets\<close>
-
-text \<open>@{thm [display,names_short, margin=40] timedOutTransaction_preserves_assets}\<close>
-
-section \<open>Positive Accounts\<close>
-
-text \<open>
-
-There are some values for State that are allowed by its type but make no sense, especially in the
-case of Isabelle semantics where we use lists of key values to represent maps:
-\begin{enumerate}
-\item The lists represent maps, so they should have no repeated keys (@{term valid_map}).
-\item Two equal maps should be represented equally, so we force keys to be in ascending order  (@{term valid_map}).
-\item Only the accounts that contain a positive amount are relevant (@{term allAccountsPositiveState}).
-\end{enumerate}
-\<close>
-
-text \<open>@{code_stmts validAndPositive_state constant: validAndPositive_state valid_state valid_map allAccountsPositive allAccountsPositiveState (Haskell)}\<close>
-
-text \<open>If the accounts are valid and possitive, then applying an input preserves that property.\<close>
-
-text \<open>@{thm [display,names_short, margin=40] playTraceAux_preserves_validAndPositive_state}\<close>
-
-section \<open>Quiescent Result\<close>
-
-text \<open>
-
-A contract is quiescent if and only if the root construct is @{term When}, or if the contract is
-@{term Close} and all accounts are empty. If an input @{term State} is valid and accounts are
-positive, then the output will be quiescent, @{const isQuiescent}.
+A contract is quiescent iff it is terminated or is awaiting an external Input \secref{sec:Quiescent}.
+If calling \<^emph>\<open>computeTransaction\<close> produces a valid \<^emph>\<open>TransactionOutput\<close>, then the continuation
+is quiescent.
 \<close>
 
 text \<open>
-
-The following always produce quiescent contracts:
-\begin{itemize}
-\item reduceContractStep \secref{sec:reduceContractStep}
-\item reduceContractUntilQuiescent \secref{sec:reduceContractUntilQuiescent}
-\item applyAllInputs  \secref{sec:applyInputs}
-\item computeTransaction  \secref{sec:computeTransaction}
-\item playTrace  \secref{sec:playTrace}
-\end{itemize}
+\<^bold>\<open>theorem\<close> \<^emph>\<open>computeTransactionIsQuiescent\<close>: @{thm [display,names_short, margin=60] computeTransactionIsQuiescent}
 \<close>
 
-text \<open>@{thm playTraceIsQuiescent}\<close>
+text \<open>The theorem and helper function are located in \<^emph>\<open>QuiescentResult.thy\<close>.\<close>
 
-section \<open>Reducing a Contract until Quiescence Is Idempotent\<close>
 
-text \<open>Once a contract is quiescent, further reduction will not change the contract or state,
+text \<open>Moreover, once a contract is quiescent, further reduction will not change the contract or state,
 and it will not produce any payments or warnings.\<close>
 
-text \<open>@{thm [display,names_short, margin=40] reduceContractUntilQuiescentIdempotent}\<close>
+text \<open>\<^bold>\<open>lemma\<close> \<^emph>\<open>reduceContractUntilQuiescentIdempotent\<close>:@{thm [display,names_short, margin=60] reduceContractUntilQuiescentIdempotent}\<close>
+
+text \<open>This lemma can be found on \<^emph>\<open>SingleInputTransactions.thy\<close>\<close>
 
 section \<open>Split Transactions Into Single Input Does Not Affect the Result\<close>
 
-text \<open>Applying a list of inputs to a contract produces the same result as applying each input
-singly.\<close>
-
-text \<open>@{thm playTraceAuxToSingleInputIsEquivalent }\<close>
-
-subsection \<open>Termination Proof\<close>
+text \<open>When we compute a transaction or play a trace we include zero or more \<^emph>\<open>Input\<close>s per \<^emph>\<open>Transaction\<close>.
+The following function expands a list of transactions with multiple Inputs into a list
+ of transactions of single input.\<close>
 
 text \<open>
-Isabelle automatically proves termination for most function. However, this is not the case for
-@{const reductionLoop}, but it is manually proved that the reduction loop monotonically reduces the
-size of the contract (except for @{term Close}, which reduces the number of accounts), this is
-sufficient to prove termination.
-
-@{thm reduceContractStepReducesSize}
+\<^item> traceListToSingleInput :: @{typeof traceListToSingleInput}
 \<close>
 
-subsection \<open>All Contracts Have a Maximum Time\label{sec:max-time-guarantee}\<close>
+text \<open>We can then prove that playing a trace with multiple inputs per transaction is the
+same as playing the trace with a single input per transaction.\<close>
 
-text \<open>If one sends an empty transaction with time equal to @{const maxTimeContract}, then the
-contract will close.\<close>
+text \<open>\<^bold>\<open>theorem\<close> \<^emph>\<open>traceToSingleInputIsEquivalent\<close>: @{thm  [display,names_short, margin=60]  traceToSingleInputIsEquivalent }\<close>
 
-text \<open>@{thm [display,names_short, margin=40] timedOutTransaction_closes_contract}\<close>
+text \<open>The helper and theorem are located in  \<^emph>\<open>SingleInputTransactions.thy\<close> \<close>
 
-subsection \<open>Contract Does Not Hold Funds After it Closes\<close>
-
-text \<open>Funds are not held in a contract after it closes.\<close>
-
-text \<open>@{thm closeIsSafe}\<close>
-
-subsection \<open>Transaction Bound\label{sec:transaction-bound}\<close>
-
-text \<open>There is a maximum number of transaction that can be accepted by a contract.\<close>
-
-(* should we have a maxTransactions :: Contract \<Rightarrow> Int in the semantics? *)
-
-text \<open>@{thm playTrace_only_accepts_maxTransactionsInitialState}\<close>
 
 (*<*)
 end

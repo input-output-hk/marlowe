d9042cc6e0e0c2003c9eff47c98896aaf080aa72
Jamie Bertram <jamie.bertram@tweag.io>
2024-01-29 10:07:41 -0500

    Fix payments check

--- marlowe-spec-test/src/Marlowe/Spec/Core/Examples/Common.hs
+++ marlowe-spec-test/src/Marlowe/Spec/Core/Examples/Common.hs
@@ -2,20 +2,32 @@
 
 module Marlowe.Spec.Core.Examples.Common (runScenario) where
 
-import Test.Tasty (TestTree)
-import Test.Tasty.HUnit (assertFailure, testCase, (@?=))
+import Marlowe.Spec.Core.Semantics (setEquals)
 import Marlowe.Spec.Interpret (InterpretJsonRequest, Request (..), parseValidResponse)
-import SemanticsTypes (Contract(..), TransactionOutput(..), txOutContract, txOutWarnings, txOutPayments, Transaction_ext (..), Payment (..))
+import SemanticsTypes (
+  Contract (..),
+  Payment (..),
+  TransactionOutput (..),
+  Transaction_ext (..),
+  txOutContract,
+  txOutPayments,
+  txOutWarnings,
+ )
+import Test.Tasty (TestTree)
+import Test.Tasty.HUnit (assertBool, assertFailure, testCase, (@?=))
 
 runScenario :: String -> Contract -> [Transaction_ext ()] -> [Payment] -> InterpretJsonRequest -> TestTree
-runScenario name contract transactions payments interpret = testCase name
+runScenario name contract transactions payments interpret =
+  testCase
+    name
     ( do
         res <- interpret $ PlayTrace 0 contract transactions
         case parseValidResponse res of
-            Left err -> assertFailure err
-            Right (TransactionError err) -> assertFailure $ "Transaction error: " ++ show err
-            Right (TransactionOutput o) -> do
-              txOutContract o @?= Close
-              txOutWarnings o @?= []
-              txOutPayments o @?= payments
+          Left err -> assertFailure err
+          Right (TransactionError err) -> assertFailure $ "Transaction error: " ++ show err
+          Right (TransactionOutput o) -> do
+            txOutContract o @?= Close
+            txOutWarnings o @?= []
+            let msg = "Expected " <> show (txOutPayments o) <> " to contain the same elements as " <> show payments
+            assertBool msg $ txOutPayments o `setEquals` payments
     )
